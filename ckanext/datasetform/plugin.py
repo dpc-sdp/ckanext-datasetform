2646e2dd19aee23c21236ad40edee91c5edbcdd7
Mark Calvert <markcalvert84@gmail.com>
2021-10-13 14:33:57 -0600

    Removed unused code

--- ckanext/datasetform/plugin.py
+++ ckanext/datasetform/plugin.py
@@ -6,31 +6,12 @@ from ckanext.datasetform import blueprint
 class DatasetformPlugin(plugins.SingletonPlugin):
     # implements(plugins.IRoutes, inherit=True)
     implements(plugins.IConfigurer, inherit=True)
-    implements(plugins.IPackageController, inherit=True)
     implements(plugins.IBlueprint)
-
-
-    # IRoutes
-
-    # def before_map(self, map):
-    #     """
-    #         /dataset/NAME/contact
-    #     """
-    #     controller = 'ckanext.datasetform.controller:ContactController'
-    #     map.connect('/dataset/{dataset_id}/contact', controller=controller, action='send')
-    #     return map
 
     # IConfigurer
 
     def update_config(self, config):
         toolkit.add_template_directory(config, 'templates')
-        toolkit.add_public_directory(config, 'public')
-        # toolkit.add_resource('fanstatic', 'datasetform')
-
-    # IPackageController
-
-    def before_view(self, pkg_dict):
-        return pkg_dict
 
     # IBlueprint
 
